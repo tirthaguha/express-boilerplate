95e3341c763daafacb610c99c84e883a9b9f6fb1
Tirtha Guha <tirtha.guha@gmail.com>
2016-12-17 01:11:52 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+/**
+ * New node file
+ */
+
+var express = require('express');
+var app = express();
+
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use('/', express.static(__dirname+'/public'));
+
+app.use(morgan('short'));
+
+var port = 8000;
+var ip = 127.0.0.1;
+
+require('dns').lookup(require('os').hostname(), function(err, add, fam) {
+  ip = add;
+  app.listen(port, function(){
+    console.log('Running on http://'+ip+':'+port);
+  });
+});
